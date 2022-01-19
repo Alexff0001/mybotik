21cc0e9bf4562b54aef5ea8e8494a590e1ed8208
Alexff <alexbadanov1@gmail.com>
2022-01-20 00:08:24 +0300

    Update test.py

--- test.py
+++ test.py
@@ -557,6 +557,7 @@ async def predl(ctx):
 @bot.event
 async def on_message(message):
     if message.channel.id == 921033910899605526:
+        role = ctx.guild.get_role(role_id = 903780351640469574)
         time = message.created_at.strftime('%Y.%m.%d %H:%M:%S')
         embed = discord.Embed(
             title ="Предложка пополнена!",
@@ -566,7 +567,7 @@ async def on_message(message):
         embed.add_field(name = "Содержимое сообщения:", value = message.content)
         embed.set_footer(text = 'Cообщение отправлено: ' + time)
         channel = bot.get_channel(931497036249980928)
-        await channel.send(f'@everyone', embed = embed)
+        await channel.send(f'{role.mention}', embed = embed)
         await message.delete()
 
     else:
